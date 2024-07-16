ca6d79e864e2aac1ef80b4edebb4a38af42d68b7
danimsteger <daniellestegertutoring@gmail.com>
2024-07-16 15:58:24 -0400

    set up queries for display tables

---
+++ db/queries.sql
@@ -0,0 +1,16 @@
+-- Departments Table
+SELECT departments.dept_id as "ID", departments.dept_name as "NAME" FROM departments;
+
+
+-- Roles Table
+SELECT roles.role_id AS "ID", roles.role_title AS "TITLE", departments.dept_name AS "DEPARTMENT", roles.role_salary AS "SALARY"
+FROM roles
+JOIN departments ON roles.dept_id = departments.dept_id;
+
+-- Employees Table
+SELECT employees.employee_id AS "ID", employees.first_name AS "FIRST NAME", employees.last_name AS "LAST NAME", roles.role_title AS "TITLE", departments.dept_name AS "DEPARTMENT", roles.role_salary AS "SALARY", employees.manager_id AS "MANAGER"
+FROM employees
+JOIN roles
+ON employees.role_id = roles.role_id
+JOIN departments
+ON roles.dept_id = departments.dept_id;
--- db/seeds.sql
+++ db/seeds.sql
@@ -13,7 +13,7 @@
             ('Lead Engineer', 150000, 1),
             ('Software Engineer', 120000, 1),
             ('Account Manager', 160000, 2),
-            ('Accountant', 1250000, 2),
+            ('Accountant', 125000, 2),
             ('Legal Team Lead', 250000, 3),
             ('Lawyer', 190000, 3);
 
