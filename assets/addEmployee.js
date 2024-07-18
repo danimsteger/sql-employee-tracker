c7e303b606b78b0c6ee57c2aa7e6db9d17ba3aee
danimsteger <daniellestegertutoring@gmail.com>
2024-07-18 17:45:08 -0400

    added manager selection to addemployee feature

--- assets/addEmployee.js
+++ assets/addEmployee.js
@@ -11,7 +11,19 @@ const pool = require("./db");
 // Get current list of roles
 async function getRoles() {
   try {
-    const result = await pool.query(`SELECT * FROM roles ORDER BY roles_id`);
+    const result = await pool.query(`SELECT * FROM roles ORDER BY role_id`);
+    const rows = result.rows;
+    return rows;
+  } catch (err) {
+    console.error("Error executing query:", err);
+  }
+}
+
+async function getEmployees() {
+  try {
+    const result = await pool.query(
+      `SELECT * FROM employees ORDER BY employee_id`
+    );
     const rows = result.rows;
     return rows;
   } catch (err) {
@@ -32,6 +44,19 @@ async function addEmployee(callback) {
       return acc;
     }, {});
 
+    const employees = await getEmployees();
+
+    // Creates array of all employees listed with their first and last names
+    const employeeNames = employees.map(
+      (employee) => `${employee.first_name} ${employee.last_name}`
+    );
+
+    // const managers = await getManagers();
+    // console.log(managers);
+
+    // const managerNames = managers.map((manager) => manager.manager_name);
+    // console.log(managerNames);
+
     const answer = await inquirer.prompt([
       {
         type: "input",
@@ -49,15 +74,28 @@ async function addEmployee(callback) {
         choices: roleNames,
         name: "role_title",
       },
+      {
+        type: "list",
+        message: "Who is the employee's manager?",
+        choices: employeeNames,
+        name: "manager_name",
+      },
     ]);
 
     // Gathers results from prompts
-    const { first_name, last_name, role_title } = answer;
+    const { first_name, last_name, role_title, manager_name } = answer;
     const role_id = roleIDs[role_title];
 
+    const selectedManager = employees.find(
+      (employee) =>
+        `${employee.first_name} ${employee.last_name}` === manager_name
+    );
+
+    const selectedManagerId = selectedManager.employee_id;
+
     // SQL syntax to add a row to employees table
-    const sql = `INSERT INTO employees (first_name, last_name, role_id) VALUES ($1, $2, $3)`;
-    const params = [first_name, last_name, role_id];
+    const sql = `INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ($1, $2, $3, $4)`;
+    const params = [first_name, last_name, role_id, selectedManagerId];
 
     // Query with SQL syntax
     await modifyTable(sql, params, callback);
